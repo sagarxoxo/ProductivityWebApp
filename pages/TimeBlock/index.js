0d70ada9f528c0a691862c55546134f35854c356
sagarxoxo <mss82605@gmail.com>
2022-11-28 22:49:19 +0530

    first commit

---
+++ pages/TimeBlock/index.js
@@ -0,0 +1,308 @@
+import React, { useEffect, useState } from 'react'
+import { Button, Col, Container, Form, Row, Table } from 'react-bootstrap'
+import styles from "../../styles/Timeblock.module.css"
+import TimePicker from 'react-time-picker/dist/entry.nostyle';
+
+export default function index() {
+
+    const [value, onChange] = useState();
+    const [dailyTask, setDailyTask] = useState(
+    [{
+        id: 0,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 1,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 2,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 3,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 4,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 5,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 6,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 7,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 8,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 9,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 10,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 11,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 12,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 13,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    {
+        id: 14,
+        taskName: "",
+        rewards: "",
+        taskCompleted: false
+    },
+    ]
+    )
+
+    useEffect(() => {
+        onChange(localStorage.getItem("Time"))
+        setDailyTask(JSON.parse(localStorage.getItem("Task")))
+    },[])
+
+    function onTimeChange(id){
+     const num = Number(value?.slice(0,2)) + id
+
+     if(num == 12 || num == 0){
+       return 12 
+     }
+     else if(num >= 12){
+        return num - 12 
+     }
+     else{
+        return num
+     }
+    
+    }
+    const saveTime = () => {
+        localStorage.setItem('Time', value);
+        localStorage.setItem('Task', JSON.stringify(dailyTask));
+    }
+
+    function handleChange(e, id){
+        const {type, checked, name, value} = e.target
+        setDailyTask(dailyTask.map(data => {
+            if (id === data.id) {
+                return { ...data, [name]: type === "checkbox" ? checked : value };
+            } else {
+                return data;
+            }
+            })
+        )
+    }
+
+  return (
+    <div>
+    <Container>
+    <Row>
+        <Col lg={6}>
+            <div className={styles.card}>
+            <div className={styles.header}>
+                <h2>Time Block</h2>
+                <div className={styles.timepc}>
+                    <TimePicker onChange={onChange} value={value}/>
+                    <Button className={styles.btnSave} onClick={saveTime}>Save</Button>
+                </div>
+            </div>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Time</th>
+                        <th>Daily Task</th>
+                        <th>Rewards</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {dailyTask?.map(taskData => {
+                    return (
+                    <tr>
+                        <td>{onTimeChange(taskData.id)}:00 </td>
+                        <td>
+                        <Form>
+                            <div className={styles.cardTask}>
+                                <Form.Group>
+                                    <Form.Check 
+                                    type="checkbox" 
+                                    name="taskCompleted" 
+                                    value={taskData.taskCompleted} 
+                                    onChange={(e) => handleChange(e, taskData.id)}
+                                    />
+                                </Form.Group>
+                                <Form.Control 
+                                placeholder="Enter Task"
+                                name="taskName" 
+                                value={taskData.taskName} 
+                                onChange={(e) => handleChange(e, taskData.id)}
+                                />
+                            </div>
+
+                        </Form>
+                        </td>
+                        <td>
+                        <Form.Control 
+                            placeholder="Enter Rewards"
+                            name="rewards" 
+                            value={taskData.rewards} 
+                            onChange={(e) => handleChange(e, taskData.id)}
+                            />
+                        </td>
+                    </tr>
+                    )
+                })}
+                </tbody>
+            </Table>
+        </div>
+        </Col>
+        <Col lg={6}>
+            
+        </Col>
+    </Row>
+    </Container>
+    </div>
+  )
+}
+
+
+
+    
+//     const dailyTask = [
+//     {
+//         id: 0,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 1,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 2,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 3,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 4,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 5,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 6,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 7,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 8,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 9,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 10,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 11,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 12,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 13,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     {
+//         id: 14,
+//         taskName: "",
+//         taskCompleted: false
+//     },
+//     ]
+    
+
+//     const [tdData, setTdData] = useState(dailyTask)
+
+//     function onTimeChange(id){
+//      const num = Number(value?.slice(0,2)) + id
+
+//      if(num == 12 || num == 0){
+//        return 12 
+//      }
+//      else if(num >= 12){
+//         return num - 12 
+//      }
+//      else{
+//         return num
+//      }
+    
+//     }
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "react-time-picker/dist/TimePicker.css"
+import "react-clock/dist/Clock.css";
+
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div>
+     <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" / >
+    </Head>
+    <Component {...pageProps} />
+  </div>
+  )
 }
 
 export default MyApp
